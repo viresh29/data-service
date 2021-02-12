90815c8f823d0c17f61024fea1b7391792aeae50
Viresh Patel <vpatel@williamhill.us>
2021-02-12 08:09:09 -0500

    added config info to main py

--- landing-club-pipeline/dags/main.py
+++ landing-club-pipeline/dags/main.py
@@ -2,6 +2,7 @@ from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
 import importlib
 from landing_club_ingest_callble import CsvToDF, SnowflakeCsvUpload
+import os
 
 params = importlib.import_module(f'params')
 
@@ -9,16 +10,24 @@ params = importlib.import_module(f'params')
 def create_dag(dag_id, config, DEFAULT_ARGS, SCHEDULE_INTERVAL):
     with DAG(dag_id, default_args=DEFAULT_ARGS, schedule_interval=SCHEDULE_INTERVAL, description='sample dag') \
             as dag:
-        csvtos3_callable = CsvToDF(file_path, file_name, from_date, to_date, bucket_name, s3_connection_id,
-                                timeout_counter,
-                 timeout_sleep)
+        csvtos3_callable = CsvToDF(config['file_path'],
+                                   config['file_name'],
+                                   config['AWS']['BUCKET'],
+                                   config['AWS']['s3_connection_id'],
+                                   config['AWS']['TIMEOUT_COUNTER'],
+                                   config['AWS']['TIMEOUT_SLEEP'])
 
         t1 = PythonOperator(task_id='ingest_csv_to_s3',
                             python_callable=csvtos3_callable,
                             provide_context=True)
 
-        ingest_snowflake = SnowflakeCsvUpload(user_name, account_name, warehouse, password, timeout_counter,
-                                          timeout_sleep)
+        ingest_snowflake = SnowflakeCsvUpload(config['SNOWFLAKE']['USER'],
+                                              config['SNOWFLAKE']['ACCOUNT'],
+                                              config['SNOWFLAKE']['WAREHOUSE'],
+                                              os.getenv('SNOWFLAKE_PASSWORD'),
+                                              config['SNOWFLAKE']['TIMEOUT_COUNTER'],
+                                              config['SNOWFLAKE']['TIMEOUT_SLEEP'],
+                                              **config['SNOWFLAKE'])
 
         t2 = PythonOperator(task_id='s3_to_snowflake',
                             python_callable=ingest_snowflake,
